8195f97e33d5394a2375a78e578d1b0c0f6ad3de
Shifa Koti <shifakoti20@gmail.com>
2025-07-27 21:16:19 +0530

    update changes

--- frontend/src/components/DifficultyPopup.jsx
+++ frontend/src/components/DifficultyPopup.jsx
@@ -3,24 +3,24 @@ import React from "react";
 const DifficultyPopup = ({ isOpen, onSelect, onClose }) => {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-opacity-40">
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black bg-opacity-40">
       <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-xs text-center relative">
         <h2 className="text-2xl font-bold mb-6 text-gray-900">Select Difficulty</h2>
-        <div className="flex flex-col gap-4 mb-4  ">
+        <div className="flex flex-col gap-4 mb-4">
           <button
-            className="px-6 py-3 bg-green-500 ring-2  text-white font-semibold rounded-xl hover:bg-green-600 transition-all"
+            className="px-6 py-3 bg-green-500 ring-2 ring-green-300 text-white font-semibold rounded-xl hover:bg-green-600 transition-all"
             onClick={() => onSelect("easy")}
           >
             Easy
           </button>
           <button
-            className="px-6 py-3 bg-yellow-500 text-white font-semibold rounded-xl hover:bg-yellow-600 transition-all"
+            className="px-6 py-3 bg-yellow-500 ring-2 ring-yellow-300 text-white font-semibold rounded-xl hover:bg-yellow-600 transition-all"
             onClick={() => onSelect("medium")}
           >
             Medium
           </button>
           <button
-            className="px-6 py-3 bg-red-500 text-white font-semibold rounded-xl hover:bg-red-600 transition-all"
+            className="px-6 py-3 bg-red-500 ring-2 ring-red-300 text-white font-semibold rounded-xl hover:bg-red-600 transition-all"
             onClick={() => onSelect("hard")}
           >
             Hard
